ea0060e30ad0e331dcf1339a435383f2b3b57c59
Rafael Horochovec <rl.horochovec@gmail.com>
2021-08-30 13:19:14 -0300

    Alterando paginação

--- src/main/java/br/rafaelhorochovec/heroi/controller/HeroiController.java
+++ src/main/java/br/rafaelhorochovec/heroi/controller/HeroiController.java
@@ -1,5 +1,6 @@
 package br.rafaelhorochovec.heroi.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.rafaelhorochovec.heroi.exception.ResourceNotFoundException;
@@ -35,16 +38,39 @@ public class HeroiController {
 	private HeroiRepository heroiRepository;
 
 	@GetMapping("/herois")
-	public Page<Heroi> getHerois(Pageable pageable) {
-		return heroiRepository.findAll(pageable);
+	public ResponseEntity<Map<String, Object>> getHerois(@RequestParam(required = false) String nome,
+			@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size) {
+
+		try {
+			List<Heroi> herois = new ArrayList<Heroi>();
+			Pageable paging = PageRequest.of(page, size);
+
+			Page<Heroi> pageHeroes;
+			if (nome == null)
+				pageHeroes = heroiRepository.findAll(paging);
+			else
+				pageHeroes = heroiRepository.findByNomeContaining(nome, paging);
+
+			herois = pageHeroes.getContent();
+
+			Map<String, Object> response = new HashMap<>();
+			response.put("herois", herois);
+			response.put("currentPage", pageHeroes.getNumber());
+			response.put("totalItems", pageHeroes.getTotalElements());
+			response.put("totalPages", pageHeroes.getTotalPages());
+
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
-	
+
 	@GetMapping("/herois/all")
 	public ResponseEntity<List<Heroi>> getAllHerois() {
 		List<Heroi> herois = heroiRepository.findAll();
 		return new ResponseEntity<>(herois, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/herois/{id}")
 	public ResponseEntity<Heroi> getHeroiById(@PathVariable(value = "id") UUID heroiId)
 			throws ResourceNotFoundException {
@@ -80,7 +106,7 @@ public class HeroiController {
 		response.put("deleted", Boolean.TRUE);
 		return response;
 	}
-	
+
 	@DeleteMapping("/herois/delete/all")
 	public ResponseEntity<HttpStatus> deleteHerois() {
 		try {
